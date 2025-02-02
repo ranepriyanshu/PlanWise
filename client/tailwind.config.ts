81e8045cb7e7a04ecf2e3d83a2cd129ee4416764
ranepriyanshu <priyanshurane09@gmail.com>
2025-02-03 01:09:06 +0530

    second commit

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,41 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#ffffff",
+        gray: {
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+        },
+
+        blue:{
+          
+          200:"#93c5fd",
+          400:"#60a5fa",
+          600:"#2563eb",
+          800:"#1d4ed8",
+          900:"#1e3a8a"
+
+        },
+        "dark-bg": "#101214",
+        "dark-secondary":"#1d1f21",
+        "dark-tertiary": "#0275ff",
+        "blue-primary": "#0275ff",
+        "stroke-dark": "2d3125",
+
+    
+
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
